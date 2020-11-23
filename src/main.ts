574b29dd639b30bb3e05c4b427647e905421df0c
Neil Kuan <guan840912@gmail.com>
2020-11-23 18:04:53 +0800

    update v2 version

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,8 @@ import * as eks from '@aws-cdk/aws-eks';
 import { App, Stack, Construct } from '@aws-cdk/core';
 import * as cdk8s from 'cdk8s';
 import { AwsLoadBalancePolicy, VersionsLists } from 'cdk8s-aws-alb-ingress-controller';
-import { MyChart } from './mycdk8s';
-
-// for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
-
-const app = new App();
-
-// new stack
-const stack = new Stack(app, 'EKSstack', {
-  env: devEnv,
-});
-
+import { MyChartV2 } from './mycdk8s';
+// exmaple for v2
 export class MainStack extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
@@ -32,12 +19,12 @@ export class MainStack extends Construct {
     });
     const sa = new eks.ServiceAccount(this, 'albserviceaccount', {
       cluster: cluster,
-      namespace: 'kube-system',
-      name: 'alb-ingress-controller',
+      name: 'aws-load-balancer-controller',
     });
-    AwsLoadBalancePolicy.addPolicy(VersionsLists.AWS_LOAD_BALANCER_CONTROLLER_POLICY_V1, sa );
-    const myChart = new MyChart(new cdk8s.App(), 'MyChart', {
-      clusterName: cluster.clusterName, replicas: 0,
+    AwsLoadBalancePolicy.addPolicy(
+      VersionsLists.AWS_LOAD_BALANCER_CONTROLLER_POLICY_V2, sa );
+    const myChart = new MyChartV2(new cdk8s.App(), 'MyChart', {
+      clusterName: cluster.clusterName,
     });
     cluster.addAutoScalingGroupCapacity('addnoManagedNG', {
       instanceType: new ec2.InstanceType('t3.medium'),
@@ -46,17 +33,77 @@ export class MainStack extends Construct {
     });
 
     const addCdk8sChart = cluster.addCdk8sChart('my-chart', myChart);
-    const albPatch = new eks.KubernetesPatch(this, 'patch-replicas-1', {
-      cluster,
-      resourceName: `deployment/${myChart.deploymentName}`,
-      resourceNamespace: myChart.deploymentNameSpace,
-      applyPatch: { spec: { replicas: 1 } },
-      restorePatch: { spec: { replicas: 0 } },
-    });
-    sa.node.addDependency(addCdk8sChart);
-    albPatch.node.addDependency(sa);
+    addCdk8sChart.node.addDependency(sa);
+
   }
 }
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
 
+const app = new App();
+
+// new stack
+const stack = new Stack(app, 'EKSstack', {
+  env: devEnv,
+});
 new MainStack(stack, 'MainStack');
 app.synth();
+
+// V1 example
+// const devEnv = {
+//   account: process.env.CDK_DEFAULT_ACCOUNT,
+//   region: process.env.CDK_DEFAULT_REGION,
+// };
+
+// const app = new App();
+
+// // new stack
+// const stack = new Stack(app, 'EKSstack', {
+//   env: devEnv,
+// });
+
+// export class MainStack extends Construct {
+//   constructor(scope: Construct, id: string) {
+//     super(scope, id);
+//     const cluster = new eks.Cluster(this, 'cdk8s-alb', {
+//       version: eks.KubernetesVersion.V1_18,
+//       defaultCapacity: 0,
+//       clusterName: 'albIngressDemo',
+//       vpc: new ec2.Vpc(this, 'newVpc', {
+//         natGateways: 1,
+//         maxAzs: 2,
+//       }),
+//     });
+//     const sa = new eks.ServiceAccount(this, 'albserviceaccount', {
+//       cluster: cluster,
+//       namespace: 'kube-system',
+//       name: 'alb-ingress-controller',
+//     });
+//     AwsLoadBalancePolicy.addPolicy(VersionsLists.AWS_LOAD_BALANCER_CONTROLLER_POLICY_V1, sa );
+//     const myChart = new MyChartV1(new cdk8s.App(), 'MyChart', {
+//       clusterName: cluster.clusterName, replicas: 0,
+//     });
+//     cluster.addAutoScalingGroupCapacity('addnoManagedNG', {
+//       instanceType: new ec2.InstanceType('t3.medium'),
+//       desiredCapacity: 1,
+//       spotPrice: '0.0416',
+//     });
+
+//     const addCdk8sChart = cluster.addCdk8sChart('my-chart', myChart);
+//     const albPatch = new eks.KubernetesPatch(this, 'patch-replicas-1', {
+//       cluster,
+//       resourceName: `deployment/${myChart.deploymentName}`,
+//       resourceNamespace: myChart.deploymentNameSpace,
+//       applyPatch: { spec: { replicas: 1 } },
+//       restorePatch: { spec: { replicas: 0 } },
+//     });
+//     sa.node.addDependency(addCdk8sChart);
+//     albPatch.node.addDependency(sa);
+//   }
+// }
+
+// new MainStack(stack, 'MainStack');
+// app.synth();
--- src/mycdk8s.ts
+++ src/mycdk8s.ts
@@ -1,16 +1,34 @@
 import * as cdk8s from 'cdk8s';
-import { AlbIngressController } from 'cdk8s-aws-alb-ingress-controller';
+import { AlbIngressController, AwsLoadBalancerController } from 'cdk8s-aws-alb-ingress-controller';
 import * as constructs from 'constructs';
 
-export interface MyChartProps {
+export interface MyChartV2Props {
+  readonly clusterName: string;
+}
+
+export class MyChartV2 extends cdk8s.Chart {
+  readonly deploymentName: string;
+  readonly deploymentNameSpace: string;
+  constructor(scope: constructs.Construct, id: string, props: MyChartV2Props) {
+    super(scope, id);
+    const alb = new AwsLoadBalancerController(this, 'alb', {
+      clusterName: props.clusterName,
+      createServiceAccount: false,
+    });
+    this.deploymentName = alb.deploymentName;
+    this.deploymentNameSpace = alb.namespace;
+  }
+}
+
+export interface MyChartV1Props {
   readonly clusterName: string;
   readonly replicas: number;
 }
 
-export class MyChart extends cdk8s.Chart {
+export class MyChartV1 extends cdk8s.Chart {
   readonly deploymentName: string;
   readonly deploymentNameSpace: string;
-  constructor(scope: constructs.Construct, id: string, props: MyChartProps) {
+  constructor(scope: constructs.Construct, id: string, props: MyChartV1Props) {
     super(scope, id);
     const alb = new AlbIngressController(this, 'alb', {
       clusterName: props.clusterName,
